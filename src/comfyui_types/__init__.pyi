1668eff71e07177ed7ff06b1c3e69fb1515bc114
Tristan Kreuziger <github@tkreuziger.com>
2024-10-24 12:19:56 +0200

    Added type hints

--- src/comfyui_types/__init__.pyi
+++ src/comfyui_types/__init__.pyi
@@ -1,31 +1,75 @@
 from .export import export_nodes as export_nodes
 from .inputs import (
-    CLIPInput as CLIPInput,
     ChoiceInput as ChoiceInput,
+)
+from .inputs import (
+    CLIPInput as CLIPInput,
+)
+from .inputs import (
     ConditioningInput as ConditioningInput,
+)
+from .inputs import (
     FloatInput as FloatInput,
+)
+from .inputs import (
     ImageInput as ImageInput,
+)
+from .inputs import (
     InputBase as InputBase,
+)
+from .inputs import (
     InputType as InputType,
+)
+from .inputs import (
     IntegerInput as IntegerInput,
+)
+from .inputs import (
     LatentInput as LatentInput,
+)
+from .inputs import (
     ModelInput as ModelInput,
+)
+from .inputs import (
     NumberDisplayMode as NumberDisplayMode,
+)
+from .inputs import (
     StringInput as StringInput,
+)
+from .inputs import (
     VAEInput as VAEInput,
 )
 from .node import ComfyUINode as ComfyUINode
 from .outputs import (
     CLIPOutput as CLIPOutput,
+)
+from .outputs import (
     ConditioningOutput as ConditioningOutput,
+)
+from .outputs import (
     FloatOutput as FloatOutput,
+)
+from .outputs import (
     ImageOutput as ImageOutput,
+)
+from .outputs import (
     IntegerOutput as IntegerOutput,
+)
+from .outputs import (
     LatentOutput as LatentOutput,
+)
+from .outputs import (
     MaskOutput as MaskOutput,
+)
+from .outputs import (
     ModelOutput as ModelOutput,
+)
+from .outputs import (
     OutputBase as OutputBase,
+)
+from .outputs import (
     StringOutput as StringOutput,
+)
+from .outputs import (
     VAEOutput as VAEOutput,
 )
 
