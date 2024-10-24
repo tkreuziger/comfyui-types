bfdcc162b063ceefe4f2135272ed60095b4b16ee
Tristan Kreuziger <github@tkreuziger.com>
2024-10-24 12:16:29 +0200

    Refactored node as metaclass to avoid class properties

--- src/comfyui_types/node.py
+++ src/comfyui_types/node.py
@@ -5,9 +5,11 @@ import json
 from .inputs import InputBase
 from .outputs import OutputBase
 
+INPUT_TYPES_TYPE = dict[str, dict[str, tuple[str | int | float]]]
 
-class ComfyUINode:
-    """Abstract base class for all typed ComfyUI nodes."""
+
+class ComfyUINodeMetaClass(type):
+    """Meta class for ComfyUINode."""
 
     function: str = 'execute'
     category: str = ''
@@ -16,15 +18,22 @@ class ComfyUINode:
     deprecated: bool = False
     experimental: bool = False
 
-    @classmethod
     def _get_display_name(cls) -> str:
         """Return display name."""
         return cls.display_name if cls.display_name != '' else cls.__name__
 
-    @classmethod
-    def INPUT_TYPES(cls) -> dict[str, dict[str, tuple]]:  # noqa: N802
+    @property
+    def FUNCTION(cls) -> str:  # noqa: N802
+        """Return entry function."""
+        return cls.function
+
+    def INPUT_TYPES(cls) -> INPUT_TYPES_TYPE:  # noqa: N802
         """Return list of input types."""
-        input_types = {'required': {}, 'optional': {}, 'hidden': {}}
+        input_types: INPUT_TYPES_TYPE = {
+            'required': {},
+            'optional': {},
+            'hidden': {},
+        }
 
         for field in cls.__dict__:
             if isinstance(getattr(cls, field), InputBase):
@@ -39,21 +48,13 @@ class ComfyUINode:
 
         return input_types
 
-    @classmethod
-    @property
-    def FUNCTION(cls) -> str:  # noqa: N802
-        """Return entry function."""
-        return cls.function
-
-    @classmethod
     @property
     def OUTPUT_NODE(cls) -> bool:  # noqa: N802
         """Whether this is an output node or not."""
         return cls.output_node
 
-    @classmethod
     @property
-    def RETURN_TYPES(cls) -> tuple[str]:  # noqa: N802
+    def RETURN_TYPES(cls) -> tuple[str, ...]:  # noqa: N802
         """Return list of return types."""
         output_types = []
         for field in cls.__dict__:
@@ -63,7 +64,6 @@ class ComfyUINode:
 
         return tuple(output_types)
 
-    @classmethod
     @property
     def RETURN_NAMES(cls) -> tuple[str]:  # noqa: N802
         """Return list of return names."""
@@ -80,19 +80,16 @@ class ComfyUINode:
 
         return tuple(output_names)  # type: ignore  # noqa: PGH003
 
-    @classmethod
     @property
     def DEPRECATED(cls) -> bool:  # noqa: N802
         """Indicated whether the node is deprecated."""
         return cls.deprecated
 
-    @classmethod
     @property
     def EXPERIMENTAL(cls) -> bool:  # noqa: N802
         """Indicated whether the node is experimental."""
         return cls.experimental
 
-    @classmethod
     def describe(cls) -> str:
         """Describe node."""
         description = f'"{cls.__name__}" in "{cls.category}"'
@@ -102,3 +99,7 @@ class ComfyUINode:
         return_names = f'Return names: {cls.RETURN_NAMES}'
 
         return f'{description}\n{intputs}\n{outputs}\n{return_names}'
+
+
+class ComfyUINode(metaclass=ComfyUINodeMetaClass):
+    """Abstract base class for all typed ComfyUI nodes."""
